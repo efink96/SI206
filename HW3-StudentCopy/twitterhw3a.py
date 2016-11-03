07402f668042b4d67c28083b9943c0c84a6ab5a2
Emma Fink <etfink@umich.edu>
2016-11-03 00:30:31 -0400

    Got twitterhw3a.py to work

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,21 @@
 
 # You will demo this live for grading.
 
+import tweepy
+
+access_token = "307613893-rH7jy1CrmHoBPOTg8aupfO352wakyAiKE8GZWB9n"
+access_token_secret = "y5FQ8j9pzdzED1XMyTfYJzdOj9cOg5ZbAhlfnlk4oyaPs"
+consumer_key = "D31bG4p7VXi2Hu7kJb2A3FaII"
+consumer_secret = "WsrTov0E25jdeGXaswhXF4CgF8Ie004sWq6w9XKnlRyjTMOUTq"
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+# Question: is it okay that I hard-coded the image?
+image = open('elle woods.jpg', 'rb')
+img = 'elle woods.jpg'
+api.update_with_media(img, status="Love Elle Woods! #UMSI-206 #Proj3 #tweetedusingpython")
+
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
